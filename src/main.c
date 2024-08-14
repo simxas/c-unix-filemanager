6a2dc16ee21f56ef9a36804d800ebe862cb5128a
simon <darbolickus@gmail.com>
2024-08-14 12:50:40 +0000

    printing current dir path

--- src/main.c
+++ src/main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+
+
+void current_dir_name() {
+    char cwd[256];
+
+    if (getcwd(cwd, sizeof(cwd)) == NULL) {
+        perror("getcwd() error");
+    } else {
+        printf("current working directory is: %s\n", cwd);
+    }
+}
 
 int main(int argc, char *argv[]) {
     //printf("Entered number of %d arguments\n", argc);
@@ -8,7 +20,8 @@ int main(int argc, char *argv[]) {
         char *arg = argv[i];
         if (strcmp(arg, "pwd") == 0) {
             printf("Entered: %s\n", arg);
-            printf("Current dir is blabla\n");
+            //printf("Current dir is blabla\n");
+            current_dir_name();
         } else {
             printf("Entered nothing good!\n");
         }
